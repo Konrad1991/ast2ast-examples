95afcfb423b08f52e129413520d77b8c87972b3f
Konrad1991 <konrad_kraemer@yahoo.de>
2024-07-23 16:07:46 +0200

    example for desolve usage with ast2ast

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# ast2ast-examples
-This repository contains example code and tutorials demonstrating use cases for ast2ast
+This repository demonstrates solving the Lorenz system using deSolve in R, with comparisons across different methods:
+
+    R Solution: Uses R code directly.
+    ast2ast: Translates R code to C++ for performance.
+    Plain C: Uses plain C code for comparison.
+
+
--- deSolve/ast2ast.cpp
+++ deSolve/ast2ast.cpp
@@ -11,15 +11,15 @@ static double parms[3];
 #define c parms[2]
 
 extern "C" {
-void initmod_a2a(void (*odeparms)(int *, double *));
+void initmod_ast2ast(void (*odeparms)(int *, double *));
 }
 
 extern "C" {
-void derivs_a2a(int *neq, double *t, double *y, double *ydot, double *yout,
-                int *ip);
+void derivs_ast2ast(int *neq, double *t, double *y, double *ydot, double *yout,
+                    int *ip);
 }
 
-void initmod_a2a(void (*odeparms)(int *, double *)) {
+void initmod_ast2ast(void (*odeparms)(int *, double *)) {
   int N = 3;
   odeparms(&N, parms);
 }
@@ -32,8 +32,8 @@ void user_fct(double t, BorrowPtr &y, BorrowPtr &ydot) {
 }
 
 /* Derivatives */
-void derivs_a2a(int *neq, double *t, double *y, double *ydot, double *yout,
-                int *ip) {
+void derivs_ast2ast(int *neq, double *t, double *y, double *ydot, double *yout,
+                    int *ip) {
   BorrowPtr y_(y, *neq);
   BorrowPtr ydot_(ydot, *neq);
   double t_ = *t;
