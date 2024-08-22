0ac4ed41a5b4982af69094baf9ef02282b4dd62d
Konrad1991 <konrad_kraemer@yahoo.de>
2024-08-22 18:33:35 +0200

    added build script

--- build_doc.sh
+++ build_doc.sh
@@ -7,9 +7,9 @@ sudo rm -r ./docs/r2sundials_files/
 sudo mv ./r2sundials/r2sundials_files ./docs/r2sundials_files/
 
 quarto render ./ParticleSwarmOptimization/pso.qmd
-mv  ./pso/pso.html ./docs/pso.html
+mv  ./ParticleSwarmOptimization/pso.html ./docs/pso.html
 sudo rm -r ./docs/pso_files/
-sudo mv ./pso/pso_files ./docs/pso_files/
+sudo mv ./ParticleSwarmOptimization/pso_files ./docs/pso_files/
 
 quarto render ./paropt/paropt.qmd
 mv  ./paropt/paropt.html ./docs/paropt.html
