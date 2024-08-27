7d80bf8ac405b8f71be1ceff271dfa38c8f23d8b
Konrad1991 <konrad_kraemer@yahoo.de>
2024-08-27 16:29:26 +0200

    added parallised pso. Added small example for dot product

--- ParticleSwarmOptimization/PSO.cpp
+++ ParticleSwarmOptimization/PSO.cpp
@@ -1,7 +1,11 @@
+// TODO: add parallel pso in order to show that translated functions can be called in parallel
+
 // [[Rcpp::depends(ast2ast)]]
 // [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(RcppThread)]]
 // [[Rcpp::plugins(cpp2a)]]
 #include "etr.hpp"
+#include <RcppThread.h>
 
 using namespace Rcpp;
 using namespace arma;
@@ -188,3 +192,124 @@ List pso_xptr(const vec &lb, const vec &ub, Rcpp::XPtr<FP> loss_xptr, int ngen,
   return List::create(Named("insilico") = insilico,
                       Named("global_best_vec") = global_best_vec);
 }
+
+
+
+
+
+// [[Rcpp::export]]
+List pso_xptr_parallel(const vec &lb, const vec &ub, Rcpp::XPtr<FP> loss_xptr, int ngen,
+              int npop, double error_threshold) {
+  FP loss = *loss_xptr;
+  int npar = lb.n_elem;
+  mat swarm = randu<mat>(npop, npar);
+  mat v = zeros<mat>(npop, npar);
+  vec swarm_bests = zeros<vec>(npop);
+  vec swarm_errors = zeros<vec>(npop);
+
+  const double initial_cog = 2.5;
+  const double final_cog = 0.5;
+  const double initial_soc = 0.5;
+  const double final_soc = 2.5;
+  const double w_max = 0.9;
+  const double w_min = 0.4;
+
+  std::vector<etr::Vec<double>> params(npop);
+  for (size_t i = 0; i < params.size(); i++) {
+    params[i] = etr::Vec<double>(etr::SI{npar});
+  }
+
+  for (int i = 0; i < npop; ++i) {
+    swarm.row(i) = (randu<rowvec>(npar) % (ub.t() - lb.t())) + lb.t();
+    for (size_t j = 0; j < npar; j++) {
+      params[i][j] = swarm.row(i)(j);
+    }
+    swarm_errors[i] = loss(params[i]);
+    swarm_bests[i] = swarm_errors[i];
+  }
+
+  uword global_best = index_min(swarm_bests);
+  rowvec global_best_vec = swarm.row(global_best);
+  double global_best_error = swarm_bests[global_best];
+  mat swarm_best_params = swarm;
+
+  int number_threads = std::thread::hardware_concurrency() - 1;
+  RcppThread::ThreadPool pool(number_threads);
+  std::vector<std::future<double>> futures(npop);
+
+  auto correctBounds = [](const rowvec &target, const vec &lb, const vec &ub) {
+    rowvec res(target.size());
+    for (size_t i = 0; i < lb.size(); i++) {
+      if (target(i) < lb(i)) {
+        res(i) = lb(i);
+      } else if (target(i) > ub(i)) {
+        res(i) = ub(i);
+      } else {
+        res(i) = target(i);
+      }
+    }
+    return res;
+  };
+
+  int iter = 0;
+  while (iter < ngen) {
+    double w = w_max - iter * (w_max - w_min) / ngen;
+    double cog = initial_cog - (initial_cog - final_cog) * (iter + 1) / ngen;
+    double soc = initial_soc - (initial_soc - final_soc) * (iter + 1) / ngen;
+
+    for (int i = 0; i < npop; ++i) {
+      rowvec local_best_vec = swarm.row(global_best);
+
+      v.row(i) =
+          w * v.row(i) +
+          cog * randu<double>() * (swarm_best_params.row(i) - swarm.row(i)) +
+          soc * randu<double>() * (local_best_vec - swarm.row(i));
+      swarm.row(i) += v.row(i);
+
+      swarm.row(i) = correctBounds(swarm.row(i), lb, ub);
+    }
+
+    for (int i = 0; i < npop; ++i) {
+      for (size_t j = 0; j < npar; j++) {
+        params[i][j] = swarm.row(i)(j);
+      }
+    }
+
+    for (int i = 0; i < npop; ++i) {
+      futures[i] = pool.pushReturn(loss, std::ref(params[i]));
+    }
+
+    for (int i = 0; i < npop; ++i) {
+      swarm_errors[i] = futures[i].get();
+    }
+
+    for (int i = 0; i < npop; i++) {
+      if (!std::isinf(swarm_errors[i]) &&
+        !std::isnan(swarm_errors[i]) &&
+        swarm_errors[i]< swarm_bests[i]) {
+        swarm_bests[i] = swarm_errors[i];
+        swarm_best_params.row(i) = swarm.row(i);
+      }
+      if (!std::isinf(swarm_errors[i]) && !std::isnan(swarm_errors[i]) &&
+          swarm_errors[i] < global_best_error) {
+        global_best = i;
+        global_best_vec = swarm.row(i);
+        global_best_error =swarm_errors[i];
+      }
+    }
+
+    iter++;
+
+    if (global_best_error < error_threshold) {
+      break;
+    }
+  }
+
+  for (size_t j = 0; j < npar; j++) {
+    params[0][j] = global_best_vec(j);
+  }
+  double insilico = loss(params[0]);
+  return List::create(Named("insilico") = insilico,
+                      Named("global_best_vec") = global_best_vec);
+}
+
--- ParticleSwarmOptimization/benchmark.R
+++ ParticleSwarmOptimization/benchmark.R
@@ -1,4 +1,55 @@
-setwd("/home/konrad/Documents/GitHub/RProjects/ast2ast_supplement/ast2ast/.development/")
+dot_product <- function(v1, v2) {
+  result = 0.0;
+  for (i in 1:length(v1)) {
+    result = result + v1[i] * v2[i];
+  }
+  return(result)
+}
+
+dot_product <- function(v1, v2) {
+  result = 0.0;
+  sz <- length(v1)
+  i <- 1
+  while(i <= sz) {
+    result = result + v1[i] * v2[i];
+    i <- i + 1
+  }
+  return(result)
+}
+
+v1 <- seq(1, 10^3, 1)
+v2 <- runif(n = 10^3)
+
+dot_product_cpp <- function(v1, v2) {
+  result::double = 0.0;
+  sz::int <- length(v1)
+  i::int <- 1
+  while(i <= sz) {
+    result = result + at(v1, i) * at(v2, i);
+    i <- i + 1
+  }
+  return(result)
+}
+
+dp_cpp <- ast2ast::translate(dot_product_cpp,
+  handle_inputs = "borrow", output = "R",
+  verbose = TRUE
+)
+cat("\n")
+
+dot_product(v1, v2)
+dp_cpp(v1, v2)
+microbenchmark::microbenchmark(
+  dot_product(v1, v2),
+  dp_cpp(v1, v2)
+)
+stop("bla")
+
+
+
+
+
+setwd("/home/konrad/Documents/GitHub/RProjects/ast2ast-examples/ParticleSwarmOptimization")
 Rcpp::sourceCpp("PSO.cpp")
 
 rosenbrock <- function(parameter) {
@@ -38,8 +89,14 @@ rosenbrock_cpp <- ast2ast::translate(rosenbrock_cpp,
   verbose = TRUE
 )
 
-pso_xptr(rep(lb, 3), rep(ub, 3), rosenbrock_cpp, 1000, npop, error_threshold)
-
+pso_xptr(rep(lb, 3), rep(ub, 3),
+  rosenbrock_cpp, 1000,
+  npop, error_threshold
+)
+pso_xptr_parallel(rep(lb, 3), rep(ub, 3),
+  rosenbrock_cpp, 1000,
+  npop, error_threshold
+)
 
 r_fct <- function() {
   set.seed(1234)
@@ -51,12 +108,22 @@ cpp_fct <- function() {
   pso_xptr(rep(lb, 3), rep(ub, 3), rosenbrock_cpp, 1000, npop, error_threshold)
 }
 
+cpp_fct_parallel <- function() {
+  set.seed(1234)
+  pso_xptr_parallel(rep(lb, 3), rep(ub, 3), rosenbrock_cpp, 1000, npop, error_threshold)
+}
+
+
 res <- microbenchmark::microbenchmark(
   r_fct(),
-  cpp_fct()
+  cpp_fct(),
+  cpp_fct_parallel()
 )
 res
 getwd()
 pdf("RosenbrockBenchmark.pdf")
 boxplot(res)
 dev.off()
+
+
+
